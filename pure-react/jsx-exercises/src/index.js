78df4a6384700b5edaca03ea42a30f3ad5eb236f
Samuel Chittenden <sbchittenden@gmail.com>
2017-10-26 14:23:05 -0600

    Added pure-react dir for working through the book

---
+++ pure-react/jsx-exercises/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Greeting() {
+
+
+
+  return (
+   'hello world'
+  );
+
+
+}
+
+
+ReactDOM.render(<Greeting/>, document.querySelector('#root'));
